b7163891793f1ef2b3c7c069a5de284a0f6c912e
ilkerkocael <ilkerkocael@gmail.com>
2025-12-07 12:20:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ load_dotenv()
 class Config:
 
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-    PRIMARY_MODEL = "models/gemini-2.5-flash"
-    FALLBACK_MODEL = "models/gemini-2.5-pro"
+    PRIMARY_MODEL = "models/gemini-flash-latest"
+    FALLBACK_MODEL = "models/gemini-pro-latest"
 
     if not GEMINI_API_KEY:
         # This is just a warning for development; in production, you might want to raise an error
 
         print("WARNING: GEMINI_API_KEY is not set in environment variables.")
+
 
